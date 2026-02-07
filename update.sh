2d2432fef31e2f23c2de1055082d382f444231e4
Bulatncq65 <emmanuelBulatncq65@gmail.com>
2026-02-07 14:04:46 +0800

    dd

--- update.sh
+++ update.sh
@@ -134,7 +134,7 @@ remove_unwanted_packages() {
         "luci-app-passwall" "luci-app-ddns-go" "luci-app-rclone" "luci-app-ssr-plus"
         "luci-app-vssr" "luci-app-daed" "luci-app-dae" "luci-app-alist" "luci-app-homeproxy"
         "luci-app-haproxy-tcp" "luci-app-openclash" "luci-app-mihomo" "luci-app-appfilter"
-        "luci-app-msd_lite" "luci-app-unblockneteasemusic" "luci-app-timewol"
+        "luci-app-msd_lite" "luci-app-unblockneteasemusic"
     )
     local packages_net=(
         "haproxy" "xray-core" "xray-plugin" "dns2socks" "alist" "hysteria"
@@ -213,8 +213,7 @@ install_small8() {
         luci-app-quickstart luci-app-istorex luci-app-cloudflarespeedtest netdata luci-app-netdata \
         lucky luci-app-lucky luci-app-openclash luci-app-homeproxy luci-app-amlogic nikki luci-app-nikki \
         tailscale luci-app-tailscale oaf open-app-filter luci-app-oaf easytier luci-app-easytier \
-        msd_lite luci-app-msd_lite cups luci-app-cupsd luci-app-timewol luci-app-wolplus luci-app-momo \
-        homebox netspeedtest speedtest-cli luci-app-netspeedtest luci-app-partexp
+        msd_lite luci-app-msd_lite cups luci-app-cupsd
 }
 
 install_passwall() {
@@ -1264,9 +1263,6 @@ PKG_MIRROR_HASH:=skip' "$makefile_path"
         sed -i 's/project(quectel-CM)$/project(quectel-CM) /' "$cmake_patch_path"
     fi
 }
-
-
-
 
 main() {
     clone_repo
